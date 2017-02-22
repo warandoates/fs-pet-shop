42226f3341e42cbb0b05079f6e0bf78e53f4820d
Michael Martinez <martinez1212@gmail.com>
2017-02-22 13:19:20 -0800

    updating repo for 2nd part of assignment

--- pets.js
+++ pets.js
@@ -18,75 +18,124 @@ switch (cmd) {
     case 'update':
         update();
         break;
+    case 'destroy':
+        destroy();
+        break;
     default:
         runErr();
 }
 
-function read() {
-    const indexParam = process.argv[3];
-    if (indexParam) {
-        fs.readFile(petsDB, 'utf8', (err, petsJSON) => {
-            if (err) throw err;
-            let pets = JSON.parse(petsJSON);
-            if (!pets[indexParam]) {
-                console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
-            } else {
-                console.log(pets[indexParam]);
-            }
-        });
-    } else {
-        fs.readFile(petsDB, 'utf8', (err, petsJSON) => {
-            if (err) throw err;
-            let pets = JSON.parse(petsJSON);
-            console.log(pets);
-        });
-    }
-}
-
-function create() {
-    fs.readFile(petsDB, 'utf8', (err, data) => {
-        if (err) throw err;
-        const pets = JSON.parse(data);
-        const age = Number(process.argv[3]);
-        const kind = process.argv[4];
-        const name = process.argv[5];
-        if (!age || !kind || !name) {
-            console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
-            process.exit(1);
-        } else {
-            pets.push({age, kind, name});
-            var petsJSON = JSON.stringify(pets);
-        }
-        fs.writeFile(petsDB, petsJSON, (writeErr) => {
-            if (writeErr) throw writeErr;
-            console.log({age, kind, name});
-        });
-    });
-}
-
-function update() {
-  fs.readFile(petsDB, 'utf8', (err, info) => {
-    if (err) throw err;
-    const pets = JSON.parse(info);
-    const index = process.argv[3];
-    const age = Number(process.argv[4]);
-    const kind = process.argv[5];
-    const name = process.argv[6];
-    if (!age || !kind || !name) {
-        console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
-        process.exit(1);
-    } else {
-      pets.splice(index, 0, {age, kind, name});
-      var petsJSON = JSON.stringify(pets);
-    }
-    fs.writeFile(petsDB, petsJSON, (writeErr) => {
-      if (writeErr) throw writeErr;
-      console.log({age, kind, name});
-    });
+function destroy() {
+  fs.readFile(petsDB, 'utf8', (readErr, info) => {
+      if (readErr) throw readErr;
+      const pets = JSON.parse(info);
+      const index = process.argv[3];
+      const age = Number(process.argv[4]);
+      const kind = process.argv[5];
+      const name = process.argv[6];
+      if (!index) {
+          console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+          process.exit(1);
+      } else {
+          pets.splice(index, 1, {
+              age,
+              kind,
+              name
+          });
+          var petsJSON = JSON.stringify(pets);
+      }
+      fs.writeFile(petsDB, petsJSON, (writeErr) => {
+          if (writeErr) throw writeErr;
+          console.log({
+              age,
+              kind,
+              name
+          });
+      });
   });
 }
 
-function runErr() {
-    console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
-    process.exit(1);
-}
+    function read() {
+        const indexParam = process.argv[3];
+        if (indexParam) {
+            fs.readFile(petsDB, 'utf8', (readErr, petsJSON) => {
+                if (readErr) throw readErr;
+                let pets = JSON.parse(petsJSON);
+                if (!pets[indexParam]) {
+                    console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+                } else {
+                    console.log(pets[indexParam]);
+                }
+            });
+        } else {
+            fs.readFile(petsDB, 'utf8', (readErr, petsJSON) => {
+                if (readErr) throw readErr;
+                let pets = JSON.parse(petsJSON);
+                console.log(pets);
+            });
+        }
+    }
+
+    function create() {
+        fs.readFile(petsDB, 'utf8', (readErr, data) => {
+            if (readErr) throw readErr;
+            const pets = JSON.parse(data);
+            const age = Number(process.argv[3]);
+            const kind = process.argv[4];
+            const name = process.argv[5];
+            if (!age || !kind || !name) {
+                console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+                process.exit(1);
+            } else {
+                pets.push({
+                    age,
+                    kind,
+                    name
+                });
+                var petsJSON = JSON.stringify(pets);
+            }
+            fs.writeFile(petsDB, petsJSON, (writeErr) => {
+                if (writeErr) throw writeErr;
+                console.log({
+                    age,
+                    kind,
+                    name
+                });
+            });
+        });
+    }
+
+    function update() {
+        fs.readFile(petsDB, 'utf8', (readErr, info) => {
+            if (readErr) throw readErr;
+            const pets = JSON.parse(info);
+            const index = process.argv[3];
+            const age = Number(process.argv[4]);
+            const kind = process.argv[5];
+            const name = process.argv[6];
+            if (!index || !age || !kind || !name) {
+                console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
+                process.exit(1);
+            } else {
+                pets.splice(index, 0, {
+                    age,
+                    kind,
+                    name
+                });
+                var petsJSON = JSON.stringify(pets);
+            }
+            fs.writeFile(petsDB, petsJSON, (writeErr) => {
+                if (writeErr) throw writeErr;
+                console.log({
+                    age,
+                    kind,
+                    name
+                });
+            });
+        });
+    }
+
+    function runErr() {
+        console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+        process.exit(1);
+    }
