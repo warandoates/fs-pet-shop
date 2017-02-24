1cc6f7bb8c347547340e62921b6aad24f108782d
Michael Martinez <martinez1212@gmail.com>
2017-02-23 20:51:15 -0800

    refactored code to use promises for all functions

--- pets.js
+++ pets.js
@@ -25,28 +25,28 @@ switch (cmd) {
     default:
         runErr();
 }
-function create() {
-  fsp.readFile(petsDB, 'utf8')
-    .then((petsJSONData) => {
-      return JSON.parse(petsJSONData);
-    })
-    .then((petsData) => {
-        const pets = petsData;
 
-        const age = Number(process.argv[3]);
-        const kind = process.argv[4];
-        const name = process.argv[5];
-        if (!age || !kind || !name) {
-            console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
-            process.exit(1);
-        }
-        pets.push({ age, kind, name });
-        console.log({ age, kind, name });
-        return JSON.stringify(pets);
+function create() {
+    fsp.readFile(petsDB, 'utf8')
+        .then((info) => {
+            return JSON.parse(info);
+        })
+        .then((petsData) => {
+            const pets = petsData;
+            const age = Number(process.argv[3]);
+            const kind = process.argv[4];
+            const name = process.argv[5];
+            if (!age || !kind || !name) {
+                console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+                process.exit(1);
+            }
+            pets.push({ age, kind, name });
+            console.log({ age, kind, name});
+            return JSON.stringify(pets);
         })
         .then((updatedJSON) => {
-        fsp.writeFile(petsDB, updatedJSON)
-    });
+            fsp.writeFile(petsDB, updatedJSON);
+        });
 }
 
 function destroy() {
@@ -66,8 +66,8 @@ function destroy() {
             console.log(deadObj);
             return JSON.stringify(pets);
         })
-        .then((petsJSON) => {
-            fsp.writeFile(petsDB, petsJSON);
+        .then((updatedJSON) => {
+            fsp.writeFile(petsDB, updatedJSON);
         });
 }
 
@@ -87,43 +87,36 @@ function read() {
             });
     } else {
         fsp.readFile(petsDB, 'utf8')
-        .then((petsJSON) => {
-            let pets = JSON.parse(petsJSON);
-            console.log(pets);
-        });
+            .then((petsJSON) => {
+                let pets = JSON.parse(petsJSON);
+                console.log(pets);
+            });
     }
 }
 
-
-
 function update() {
-    fs.readFile(petsDB, 'utf8', (readErr, info) => {
-        if (readErr) throw readErr;
-        const pets = JSON.parse(info);
-        const index = process.argv[3];
-        const age = Number(process.argv[4]);
-        const kind = process.argv[5];
-        const name = process.argv[6];
-        if (!index || !age || !kind || !name) {
-            console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
-            process.exit(1);
-        } else {
-            pets.splice(index, 0, {
-                age,
-                kind,
-                name
-            });
-            var petsJSON = JSON.stringify(pets);
-        }
-        fs.writeFile(petsDB, petsJSON, (writeErr) => {
-            if (writeErr) throw writeErr;
-            console.log({
-                age,
-                kind,
-                name
-            });
+    fsp.readFile(petsDB, 'utf8')
+        .then((info) => {
+            return JSON.parse(info);
+        })
+        .then((petsJSONData) => {
+            const pets = petsJSONData;
+            const index = process.argv[3];
+            const age = Number(process.argv[4]);
+            const kind = process.argv[5];
+            const name = process.argv[6];
+
+            if (!index || !age || !kind || !name) {
+                console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
+                process.exit(1);
+            }
+            pets.splice(index, 0, { age, kind, name });
+            console.log({ age, kind, name });
+            return JSON.stringify(pets);
+        })
+        .then((updatedJSON) => {
+            fsp.writeFile(petsDB, updatedJSON);
         });
-    });
 }
 
 function runErr() {
