148f1b626464072b7c2754854bc97c945e299045
Michael Martinez <martinez1212@gmail.com>
2017-02-24 08:32:29 -0800

    refactored post method to use promieses

--- expressServer.js
+++ expressServer.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const fs = require('fs');
-
 const path = require('path');
-const petsDB = path.join(__dirname, 'pets.json');
 
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const fsp = require('fs-promise');
+
+const petsDB = path.join(__dirname, 'pets.json');
+
+
 
 app.disable('x-powered-by');
 app.use(morgan('dev'));
@@ -16,46 +19,49 @@ app.use(bodyParser.json());
 
 
 app.get('/pets', (req, res) => {
-    fs.readFile(petsDB, 'utf8', (readErr, petsJSON) => {
-        if (readErr) {
+    fsp.readFile(petsDB, 'utf8')
+        .then((petsDBData) => {
+            let pets = JSON.parse(petsDBData);
+            res.send(pets);
+        })
+        .catch((readErr) => {
             console.error(readErr.stack);
             return res.sendStatus(500);
-        }
-        let pets = JSON.parse(petsJSON);
-        res.send(pets);
-    });
+        });
 });
 
 
 app.post('/pets', (req, res) => {
-    fs.readFile(petsDB, 'utf8', (readErr, petsJSON) => {
-        if (readErr) {
-            console.error(readErr.stack);
-            return res.sendStatus(500);
-        }
-
-        const pets = JSON.parse(petsJSON);
-
-          let name = req.body.name;
-          let age =  req.body.age;
-          let kind = req.body.kind;
+fsp.readFile(petsDB, 'utf8')
+    .catch((readErr) => {
+        console.error(readErr.stack);
+        return res.sendStatus(500);
+    })
+    .then((petsDBData) => {
+        return JSON.parse(petsDBData);
+    })
+    .then((realPetsJson) => {
+        const pets = realPetsJson;
+        const name = req.body.name;
+        const age = req.body.age;
+        const kind = req.body.kind;
 
         if (name.length === 0 || age.length === 0 || kind.length === 0) {
-          return res.sendStatus(400);
+            return res.sendStatus(400);
         }
         pets.push(req.body);
-        let updatedPetsJSON = JSON.stringify(pets);
-
-        fs.writeFile(petsDB, updatedPetsJSON, (writeErr) => {
-            if (writeErr) {
-                console.error(writeErr.stack);
-                return res.sendStatus(500);
-            }
-
-            res.send(req.body);
-        });
+        res.send(req.body);
+        return JSON.stringify(pets);
+    })
+    .then((updatedPetsJSON) => {
+        fsp.writeFile(petsDB, updatedPetsJSON);
+    })
+    .catch((writeErr) => {
+        console.error(writeErr.stack);
+        return res.sendStatus(500);
     });
 });
+
 
 app.get('/pets/:index', (req, res) => {
     fs.readFile(petsDB, 'utf8', (readErr, petsJSON) => {
