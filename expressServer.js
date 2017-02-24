1be0f75e0c697b89250abc941eb8a070fe981fdc
Michael Martinez <martinez1212@gmail.com>
2017-02-24 08:40:00 -0800

    refactored all code to use promises

--- expressServer.js
+++ expressServer.js
@@ -32,51 +32,56 @@ app.get('/pets', (req, res) => {
 
 
 app.post('/pets', (req, res) => {
-fsp.readFile(petsDB, 'utf8')
-    .catch((readErr) => {
-        console.error(readErr.stack);
-        return res.sendStatus(500);
-    })
-    .then((petsDBData) => {
-        return JSON.parse(petsDBData);
-    })
-    .then((realPetsJson) => {
-        const pets = realPetsJson;
-        const name = req.body.name;
-        const age = req.body.age;
-        const kind = req.body.kind;
+    fsp.readFile(petsDB, 'utf8')
+        .catch((readErr) => {
+            console.error(readErr.stack);
+            return res.sendStatus(500);
+        })
+        .then((petsDBData) => {
+            return JSON.parse(petsDBData);
+        })
+        .then((realPetsJson) => {
+            const pets = realPetsJson;
+            const name = req.body.name;
+            const age = req.body.age;
+            const kind = req.body.kind;
 
-        if (name.length === 0 || age.length === 0 || kind.length === 0) {
-            return res.sendStatus(400);
-        }
-        pets.push(req.body);
-        res.send(req.body);
-        return JSON.stringify(pets);
-    })
-    .then((updatedPetsJSON) => {
-        fsp.writeFile(petsDB, updatedPetsJSON);
-    })
-    .catch((writeErr) => {
-        console.error(writeErr.stack);
-        return res.sendStatus(500);
-    });
+            if (name.length === 0 || age.length === 0 || kind.length === 0) {
+                return res.sendStatus(400);
+            }
+            pets.push(req.body);
+            res.send(req.body);
+            return JSON.stringify(pets);
+        })
+        .then((updatedPetsJSON) => {
+            fsp.writeFile(petsDB, updatedPetsJSON);
+        })
+        .catch((writeErr) => {
+            console.error(writeErr.stack);
+            return res.sendStatus(500);
+        });
 });
 
 
 app.get('/pets/:index', (req, res) => {
-    fs.readFile(petsDB, 'utf8', (readErr, petsJSON) => {
-        if (readErr) {
+    fsp.readFile(petsDB, 'utf8')
+        .catch((readErr) => {
             console.error(readErr.stack);
             return res.sendStatus(500);
-        }
-        let index = Number(req.params.index);
-        let pets = JSON.parse(petsJSON);
-        if (index < 0 || index >= pets.length || Number.isNaN(index)) {
-            return res.sendStatus(404);
-        }
-        res.set('Content-Type', 'application/json');
-        res.send(pets[index]);
-    });
+        })
+        .then((petsDBData) => {
+            return JSON.parse(petsDBData);
+        })
+        .then((realPetsJson) => {
+            const pets = realPetsJson;
+            let index = Number(req.params.index);
+            if (index < 0 || index >= pets.length || Number.isNaN(index)) {
+                return res.sendStatus(404);
+            }
+            res.set('Content-Type', 'application/json');
+            res.send(pets[index]);
+
+        });
 });
 
 
